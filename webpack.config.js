80e8a4040fd5c117b44656009e09e9932692e2aa
Milan Stiglic <stiglic.231@gmail.com>
2020-09-25 21:36:46 +0200

    add css-loader & style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,27 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
-    },  // [project folder]/public/bundle.js
+    },  
     module: {
-        rules: [{
-            loader: 'babel-loader',  // transpile JavaScript files using Babel and webpack
-            test: /\.js$/,  // regular expression -> js files
-            exclude: /node_modules/
-        }]
+        rules: [
+            {
+                loader: 'babel-loader',  
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: 
+                [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ]
+            },
+        ]
     },
-    devtool: 'cheap-module-eval-source-map',  // source-map
+    devtool: 'cheap-module-eval-source-map', 
     devServer: {
-        contentBase: path.join(__dirname, 'public')  // [project folder]/public
+        contentBase: path.join(__dirname, 'public')
     }
 };
